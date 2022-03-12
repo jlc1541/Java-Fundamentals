fe1028f04c3edd7fabb398118b3e69d80b6b634d
jlc1541 <70033185+jlc1541@users.noreply.github.com>
2022-03-12 17:22:22 -0600

    fund exercises complete

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -14,8 +14,20 @@ public class Exercise_04 {
 
         int i = 1;
         System.out.println("int i is: " + i);
-
-        // write your code below
+        boolean tOrf = false;
+        System.out.println("boolean tOrf is: " + tOrf);
+        char ltr = 'b';
+        System.out.println("char ltr is: " + ltr);
+        byte byt = 126;
+        System.out.println("Byte byt is: " + byt);
+        short shr = 31000;
+        System.out.println("Short shr is: " + shr);
+        float flt = 1.12121f;
+        System.out.println("float flt is: " + flt);
+        double dub = 1.8697869687;
+        System.out.println("Double dub is: " +dub);
+        long lng = 1293810928310L;
+        System.out.println("Long lng is: " + lng);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,23 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int x = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean bl = str.equals(str2);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String var = str + str2;
+
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        int ind = var.indexOf("!");
+        boolean cont = var.contains("hel");
+        String ins = var.substring(0, 3);
+        String repl = var.replace('h','b');
+        
 
     }
 
